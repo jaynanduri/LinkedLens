1519601c88484b8955eeccf3ed0991b6a76df2ab
apeksha-joshi <apeksha.suhas.joshi@gmail.com>
2025-03-21 16:24:26 -0400

    add fastapi endpoint for chat

--- model-development/src/main.py
+++ model-development/src/main.py
@@ -1,54 +1,41 @@
+import uvicorn
+import asyncio
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from logger import logger
-from config.settings import settings
-from langgraph.graph import StateGraph
-from graph.state import State
-from langchain.schema import HumanMessage, AIMessage
-from graph.graph_builder import Graph
+from datetime import datetime
+from endpoints import router
+import json
 
-def chat_loop(graph: StateGraph, initial_state: State, config:dict):
-    state = initial_state
-    print("Chat session started. Type 'exit' to end the conversation.")
-    
-    while True:
-        user_input = input("User: ").strip()
-        
-        if user_input.lower() == 'exit':
-            print("Ending chat session.")
-            break
-        
-        state["query"] = user_input
-        state["messages"].append(HumanMessage(content=user_input))
+app = FastAPI()
 
-        # Process through the graph
-        for step in graph.stream(state, config=config, stream_mode="values"):
-          state = step
-            
-        if state.get("response"):
-            print(f"Assistant: {state['response']}")
-        
-        # Reset query and response for the next iteration.
-        state["query"] = ""
-        state["response"] = ""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    print(f"Final state: {state}")
 
-def create_default_state() -> State:
-  """Creates a default State dictionary."""
-  return {
-      "query": "",
-      "standalone_query": "",
-      "query_type": "",
-      "vector_namespace": [],
-      "retrieved_docs": [],
-      "final_context": "",
-      "response": "",
-      "messages": []
-  }
+@app.middleware("http")
+async def log_requests(request, call_next):
+    start_time = datetime.now()
+    response = await call_next(request)
+    process_time = (datetime.now() - start_time).total_seconds()
+    log_data = {
+        "timestamp": datetime.now().isoformat(),
+        "method": request.method,
+        "url": str(request.url),
+        "status_code": response.status_code,
+        "process_time": process_time,
+        "client_ip": request.client.host,
+    }
+    logger.info(json.dumps(log_data))
+    return response
+
+
+app.include_router(router)
 
 if __name__ == "__main__":
-    config_user1 = {"configurable": {"thread_id": "user1"}}
-    state = create_default_state()
-    graph_builder = Graph()
-    # build graph 
-    graph = graph_builder.build_graph(isMemory=True)
-    chat_loop(graph, state, config_user1)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
