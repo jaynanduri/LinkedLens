c75b9d2c929a9955288114323fdea7d44afb527a
sravanthinittala <nittala.d@northeastern.edu>
2025-04-20 00:40:21 -0400

    chore: add k8s files

--- infra/scripts/startup_gke.sh
+++ infra/scripts/startup_gke.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+gcloud container clusters get-credentials linkedlens-cluster --region us-east1 --project linkedlens-452503
+
+kubectl apply -f /kubernetes/namespace.yaml
+
+kubectl apply -f /kubernetes/model-svc.yaml
+
+kubectl annotate serviceaccount \
+  linkedlens-ksa \
+  --namespace app \
+  iam.gke.io/gcp-service-account=firestoreserviceaccount@linkedlens.iam.gserviceaccount.com
