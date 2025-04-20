033787606b306fc0e037821134db5f88dd916a06
sravanthinittala <nittala.d@northeastern.edu>
2025-04-20 17:08:59 -0400

    fix: scripts

--- infra/scripts/startup_gke.sh
+++ infra/scripts/startup_gke.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcloud container clusters get-credentials linkedlens-cluster --region us-east1 --project linkedlens-452503
+gcloud container clusters get-credentials linkedlens-cluster --zone us-east1-d --project linkedlens-452503
 
-kubectl apply -f /kubernetes/namespace.yaml
+kubectl apply -f ./kubernetes/model-svc.yaml
 
-kubectl apply -f /kubernetes/model-svc.yaml
+kubectl apply -f ./kubernetes/model-svc.yaml
 
 kubectl annotate serviceaccount \
   linkedlens-ksa \
