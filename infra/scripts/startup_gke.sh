485e3f5fc08dcbe53233613260d5c63fe69ac1b0
sravanthinittala <nittala.d@northeastern.edu>
2025-04-20 21:41:45 -0400

    chore: updates

--- infra/scripts/startup_gke.sh
+++ infra/scripts/startup_gke.sh
@@ -2,11 +2,8 @@
 
 gcloud container clusters get-credentials linkedlens-cluster --zone us-east1-d --project linkedlens-452503
 
-kubectl apply -f ./kubernetes/model-svc.yaml
+kubectl apply -f ./kubernetes/namespace.yaml
 
 kubectl apply -f ./kubernetes/model-svc.yaml
 
-kubectl annotate serviceaccount \
-  linkedlens-ksa \
-  --namespace app \
-  iam.gke.io/gcp-service-account=firestoreserviceaccount@linkedlens.iam.gserviceaccount.com
+kubectl annotate serviceaccount linkedlens-ksa --namespace app iam.gke.io/gcp-service-account=firestoreserviceaccount@linkedlens.iam.gserviceaccount.com
